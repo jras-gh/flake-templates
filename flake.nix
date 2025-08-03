f79ef4db18fb5cc186e8014bb27127653c4f761d
Jasper Ras <jaspert.ras@gmail.com>
2025-08-03 17:20:50 +0200

    do add it the the root flake

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
         path = ./python-project;
         description = "A python project using devenv";
       };
+      rust-project = {
+        path = ./rust-project;
+        description = "A rust project";
+      };
     };
   };
 }
